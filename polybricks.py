d535b63770d6bf7b5f12929e1c0f7987a91c8771
Thomas Marchand <thomas.marchand44@gmail.com>
2019-05-11 17:08:14 +0200

    method to rotate rectangles updated

--- polybricks.py
+++ polybricks.py
@@ -13,9 +13,12 @@ STATE_PLAYING = 1
 STATE_WON = 2
 STATE_GAME_OVER = 3
 
-RECTANGLES = []
+RECTANGLES_NUMBER = 8
+RECTANGLES = {}
+SELECTED_RECTANGLE_ID = 0
 
-BRICK_COLOR = (0, 255, 0)
+DEFAULT_BRICK_COLOR = (0, 255, 0)
+SELECTED_BRICK_COLOR = (255, 255, 0)
 
 
 class PolyBricks:
@@ -48,13 +51,35 @@ class PolyBricks:
         return ((point[0] * math.cos(orientation) - point[1] * math.sin(orientation))+landmark[0],
                 (point[0] * math.sin(orientation) + point[1] * math.cos(orientation))+landmark[1])
 
-    def draw_rectangle(self, x, y, width, length, orientation, color):
+    def get_rectangle(self, x, y, width, length, orientation):
         pointlist = [(x,y)]
         pointlist.append(self.rotate_point((x,y), (width, 0), orientation))
         pointlist.append(self.rotate_point((x, y),  (width, length), orientation))
         pointlist.append(self.rotate_point((x,y), (0, length), orientation))
-        pygame.draw.polygon(self.screen, color, pointlist, 0)
+        return pointlist
 
+    def show_rectangles(self):
+        if RECTANGLES:
+            for rectangle_id in RECTANGLES:
+                color = SELECTED_BRICK_COLOR if rectangle_id == SELECTED_RECTANGLE_ID else DEFAULT_BRICK_COLOR
+                pygame.draw.polygon(self.screen, color, RECTANGLES[rectangle_id], 0)
+        else:
+            self.init_rectangles()
+            
+    def init_rectangles(self):
+        center = [x/2 for x in SCREEN_SIZE]
+        radius = SCREEN_SIZE[1]/4
+        side = 2*radius*math.sin(math.pi/RECTANGLES_NUMBER)
+        thickness = 50
+        angle = 360/RECTANGLES_NUMBER
+        RECTANGLES[0] = self.get_rectangle(center[0]-side/2, center[1]-radius, side, thickness, 0)
+        #RECTANGLES[0][1][0] -> x of 3nd point of rectangle with id 0
+        print(RECTANGLES[0])
+        i = 1
+        RECTANGLES[i] = self.get_rectangle(RECTANGLES[i-1][1][0], RECTANGLES[i-1][0][1], side, thickness, angle)
+
+        self.show_rectangles()
+        
 
     def run(self):
         while 1:
@@ -63,16 +88,11 @@ class PolyBricks:
                     quit()
 
             self.screen.fill(BACKGROUND)
-            self.draw_rectangle(
-                SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2, 150, 50, 45, BRICK_COLOR)
-            self.draw_rectangle(
-                SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2, 150, 50, 0, TEXT_FILL)
+            self.show_rectangles()
             self.show_stats()
             pygame.display.flip()
 
 
 if __name__ == "__main__":
-
-    rectangles_number = 8  # int(input("Number of rectangles:"))
-
+    RECTANGLES_NUMBER = 8  # int(input("Number of rectangles:"))
     PolyBricks().run()
